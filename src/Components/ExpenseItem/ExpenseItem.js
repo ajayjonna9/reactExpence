1d29c1b58ba2641959455929520b530d39809935
ajayjonna9 <100123937+ajayjonna9@users.noreply.github.com>
2022-11-22 12:57:35 +0530

    deleteing expense card

--- src/Components/ExpenseItem/ExpenseItem.js
+++ src/Components/ExpenseItem/ExpenseItem.js
@@ -4,12 +4,19 @@ import ExpenceDate from "../ExpenseData/ExpenceDate";
 import ExpenseDetails from "../ExpenseDetails/ExpenseDetails";
 import Card from "../Card/Card";
 const ExpenseItem = (props) => {
+  const onDelete = (e) => {
+    e.preventDefault();
+    console.log(e);
+    e.target.parentElement.remove();
+  };
   return (
     <Card className="expense-item">
       <ExpenceDate date={props.date} />
       <ExpenseDetails amount={props.cost} title={props.title} />
+      <button onClick={onDelete}>Delete</button>
     </Card>
   );
 };
 
 export default ExpenseItem;
+
