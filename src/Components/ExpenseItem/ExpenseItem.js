afe66a92e3ad6499a9fe964555d6fd9a48175095
ajayjonna9 <100123937+ajayjonna9@users.noreply.github.com>
2022-11-22 15:41:29 +0530

    update price

--- src/Components/ExpenseItem/ExpenseItem.js
+++ src/Components/ExpenseItem/ExpenseItem.js
@@ -1,22 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Expenceitems.css";
 import ExpenceDate from "../ExpenseData/ExpenceDate";
 import ExpenseDetails from "../ExpenseDetails/ExpenseDetails";
 import Card from "../Card/Card";
 const ExpenseItem = (props) => {
-  const onDelete = (e) => {
+  const [price, setPrice] = useState(props.cost);
+  const onChangePrice = (e) => {
     e.preventDefault();
-    console.log(e);
-    e.target.parentElement.remove();
+    setPrice(100);
   };
   return (
     <Card className="expense-item">
       <ExpenceDate date={props.date} />
-      <ExpenseDetails amount={props.cost} title={props.title} />
-      <button onClick={onDelete}>Delete</button>
+      <ExpenseDetails amount={price} title={props.title} />
+      <button onClick={onChangePrice}>Delete</button>
     </Card>
   );
 };
 
 export default ExpenseItem;
-
